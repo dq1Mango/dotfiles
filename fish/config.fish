b8761cff5c3cb6e54757bec02ca2cba8e1934a33
Thomas Ranney <thomas@ranney.com>
2025-07-25 09:27:42 -0400

    bits and pieces here and there

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,23 @@
 starship init fish | source
 
 # enviroment variables
+set -gx GTK_THEME catppuccin-mocha-lavender-standard+default
+
+# additions to path 
 fish_add_path /home/dq1mango/.scripts/
 
-#command aliases
+# command aliases
+# 
+# mostly handled by shells.nix
+#
+#
+
+
+# source the various other functions
+#
+set -l path "$__fish_config_dir/"functions/
+
+for func in $(ls $path)
+  source $path$func # im starting to like this whole fish thing
+end
+
