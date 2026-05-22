66a29c4ce5e53a78a0a2a598c33878e81f62e783
Thomas Ranney <thomas@ranney.com>
2026-05-22 17:31:17 -0400

    added the vpn script to the config, and aldo changed some hyprland stuff apparently

---
+++ shared/waybar/scripts/vpn.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+MULLVAD=false
+
+display-output() {
+  echo "hi"
+}
+
+check-mullvad() {
+
+  status=$(mullvad status | grep "Connected")
+  # echo $status
+
+  if [[ $status == "Connected" ]] then
+    # echo "it worked"
+    MULLVAD=true
+
+  fi
+
+}
+
+# display-output
+check-mullvad
+
+if [[ $MULLVAD == true ]] then
+  echo "󰒄 "
+else 
+  echo "󰩠 "
+fi
+
